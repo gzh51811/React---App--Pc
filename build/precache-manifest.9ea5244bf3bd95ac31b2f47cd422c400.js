e659525b6087e516dc34a22fd24a5dc0e9a47749
Ann <446331381@qq.com>
2019-04-11 20:44:13 +0800

    文件打包

---
+++ build/precache-manifest.9ea5244bf3bd95ac31b2f47cd422c400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6881e67a3bb7fc04c265",
+    "url": "/static/js/main.060d5592.chunk.js"
+  },
+  {
+    "revision": "0b864ef5ee3b3dea8bfd",
+    "url": "/static/js/2.d6f841ec.chunk.js"
+  },
+  {
+    "revision": "6881e67a3bb7fc04c265",
+    "url": "/static/css/main.92782bea.chunk.css"
+  },
+  {
+    "revision": "0b864ef5ee3b3dea8bfd",
+    "url": "/static/css/2.36b88932.chunk.css"
+  },
+  {
+    "revision": "0193c9d8de2f39e45fe12994fdfe8b22",
+    "url": "/index.html"
+  }
+];
